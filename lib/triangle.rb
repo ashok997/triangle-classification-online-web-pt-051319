e0001aa3fed5f6530434141735cc32748d7557fe
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-18 02:31:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,11 +11,11 @@ class Triangle
   end
   
  
-    if side_a==0||side_b==0||side_c==0
+  if @side_a==0||@side_b==0||@side_c==0
       raise TriangleError
-    elsif side_a<0 ||side_b<0||side_c<0
+    elsif @side_a<0 ||@side_b<0||@side_c<0
       raise TriangleError
-    elsif side_a+side_b<side_b||side_b+side_c<side_a
+    elsif @side_a+@side_b<@side_b||@side_b+@side_c<@side_a
       raise TriangleError
     else
       self.kind
