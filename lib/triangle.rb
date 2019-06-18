88c1ea2b9f600ca12205b9f4940caf44b20190cd
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-18 02:32:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,24 +2,16 @@ require 'pry'
 
 class Triangle
   # write code here
-  attr_accessor :side_a, :side_b, :side_c
+  attr_accessor :a, :b, :c
   
-  def initialize (side_a, side_b, side_c)
-    @side_a = side_a
-    @side_b = side_b
-    @side_c = side_c
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
   
  
-  if @side_a==0||@side_b==0||@side_c==0
-      raise TriangleError
-    elsif @side_a<0||@side_b<0||@side_c<0
-      raise TriangleError
-    elsif @side_a+@side_b<@side_b||@side_b+@side_c<@side_a
-      raise TriangleError
-    else
-      self.kind
-   end
+ 
   
   def kind
 
