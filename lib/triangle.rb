5f104974ec67bcbb735e55d0fb428a904304b15f
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-18 01:53:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,8 +24,9 @@ class Triangle
    
   
   def kind
-   # binding.pry
-    if side_a==side_b && side_b==side_c
+   if illegal==true
+     raise TriangleError
+    elsif side_a==side_b && side_b==side_c
       :equilateral
     elsif side_a==side_b || side_b==side_c ||side_a==side_c
       :isosceles
