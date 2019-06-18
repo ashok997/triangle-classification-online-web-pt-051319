6ccfc904c80039a377a93a1c82ab19f657ff3e52
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-18 01:09:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,13 @@
 class Triangle
   # write code here
+  attr_accessor :side_a, :side_b, :side_c
+  
+  
+  
+  def kind
+    if side_a==side_b && side_b==side_c
+      
+  end
   
   class TriangleError < StandardError
     
